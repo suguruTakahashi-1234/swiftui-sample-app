c5644c095ccd238d17872be9f0ea9ab02ff58ef0
Suguru Takahashi <60458956+suguruTakahashi-1234@users.noreply.github.com>
2023-05-01 19:35:36 +0900

    ライトモードとダークモードのテストはテストプランで設定するように変更する

--- SampleAppRelease/SampleAppReleaseUITests/SampleAppReleaseUITestsLaunchTests.swift
+++ SampleAppRelease/SampleAppReleaseUITests/SampleAppReleaseUITestsLaunchTests.swift
@@ -10,7 +10,7 @@ import XCTest
 final class SampleAppReleaseUITestsLaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
+        false
     }
 
     override func setUpWithError() throws {
