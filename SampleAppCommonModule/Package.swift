c23d8fcb067cece479cc65fa0200e7ba3bd00af3
Suguru Takahashi <60458956+suguruTakahashi-1234@users.noreply.github.com>
2023-05-02 13:56:07 +0900

    [ci skip] コメント変更

--- SampleAppCommonModule/Package.swift
+++ SampleAppCommonModule/Package.swift
@@ -20,7 +20,6 @@ let package = Package(
     defaultLocalization: "ja",
     platforms: [.iOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "SampleAppCommonModule", targets: ["SampleAppCommonModule"]),
         .library(name: "SampleAppCoreFoundation", targets: ["SampleAppCoreFoundation"]),
         .library(name: "SampleAppDomain", targets: ["SampleAppDomain"]),
@@ -29,16 +28,12 @@ let package = Package(
         .library(name: "SampleAppPresentation", targets: ["SampleAppPresentation"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/realm/SwiftLint", branch: "main"),
         .package(url: "https://github.com/Quick/Quick", branch: "main"),
         .package(url: "https://github.com/Quick/Nimble", branch: "main"),
         .package(url: "https://github.com/nicklockwood/SwiftFormat", branch: "master"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SampleAppCoreFoundation",
             dependencies: [],
