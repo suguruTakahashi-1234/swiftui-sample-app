b6d81c4b2b7eaec2702578e2a247e1ac85e97e10
Suguru Takahashi <samonikura100@gmail.com>
2023-05-06 19:22:25 +0900

    #11 カスタム属性の設定

--- SampleAppDebug/SampleAppDebug/SampleAppDebugApp.swift
+++ SampleAppDebug/SampleAppDebug/SampleAppDebugApp.swift
@@ -7,6 +7,7 @@
 
 import FirebaseAnalytics
 import FirebaseCore
+import FirebaseCrashlytics
 import SwiftUI
 
 @main
@@ -23,7 +24,13 @@ struct SampleAppDebugApp: App {
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         FirebaseApp.configure()
+
+        // Firebase Crashlytics サンプルカスタムvalue設定
+        Crashlytics.crashlytics().setCustomValue("sample_str_key", forKey: "str_key")
+
+        // Firebase Analytics サンプルイベント送信
         Analytics.logEvent("sample_event", parameters: nil)
+
         return true
     }
 }
